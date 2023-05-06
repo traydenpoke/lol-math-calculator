a50cf0f3c27132138963480bfa0d111d3fa50158
Trayden <traybouc@hotmail.com>
2023-05-06 03:56:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var data;
+let data;
 
 function saveData() {
     data = document.getElementsByClassName("input-text");
@@ -9,7 +9,7 @@ function saveData() {
         }
     }
 
-    var text = document.getElementById("text");
+    let text = document.getElementById("text");
     text.textContent = calculate();
 
 }
@@ -17,23 +17,23 @@ function saveData() {
 function calculate() {
     const cost = data[15].value;
 
-    var value = 0;
+    let value = 0;
 
-    value += data[0].value / 10 * 350;
-    value += data[1].value / 12 * 300;
-    value += data[2].value / 10 * 291.5235;
-    value += data[3].value / 18 * 750;
-    value += data[4].value / 7 * 375;
-    value += data[5].value / 15 * 600;
-    value += data[6].value / 20 * 435;
-    value += data[7].value / 18 * 560;
-    value += data[8].value / 13 * 706.25;
-    value += data[9].value / 10 * 266.67;
-    value += data[10].value / 250 * 350;
-    value += data[11].value / 150 * 400;
-    value += data[12].value / 15 * 300;
-    value += data[13].value / 25 * 450;
-    value += data[14].value / 25 * 300;
+    value += data[0].value / 10 * 350;          // Attack Damage
+    value += data[1].value / 12 * 300;          // Attack Speed
+    value += data[2].value / 10 * 291.5235;     // Lethality
+    value += data[3].value / 18 * 750;          // % Armor Pen
+    value += data[4].value / 7 * 375;           // Lifesteal
+    value += data[5].value / 15 * 600;          // Crit Chance
+    value += data[6].value / 20 * 435;          // Ability Power
+    value += data[7].value / 18 * 560;          // Flat Magic Pen
+    value += data[8].value / 13 * 706.25;       // % Magic Pen
+    value += data[9].value / 10 * 266.67;       // Ability Haste
+    value += data[10].value / 250 * 350;        // Mana
+    value += data[11].value / 150 * 400;        // Health
+    value += data[12].value / 15 * 300;         // Armor
+    value += data[13].value / 25 * 450;         // Magic Resistance
+    value += data[14].value / 25 * 300;         // Movement Speed
 
 
     const goldValue = (value / cost * 100).toFixed(2);
