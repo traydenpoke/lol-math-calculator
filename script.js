405ddabb835df8f95d142b26cc844cd0684f3f09
Trayden <traybouc@hotmail.com>
2023-05-09 03:04:36 -0400

    add more stats

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function saveData() {
 }
 
 function calculate() {
-    const cost = data[15].value;
+    const cost = data[19].value;
 
     let value = 0;
 
@@ -24,16 +24,20 @@ function calculate() {
     value += data[2].value / 10 * 291.5235;     // Lethality
     value += data[3].value / 18 * 750;          // % Armor Pen
     value += data[4].value / 7 * 375;           // Lifesteal
-    value += data[5].value / 15 * 600;          // Crit Chance
-    value += data[6].value / 20 * 435;          // Ability Power
-    value += data[7].value / 18 * 560;          // Flat Magic Pen
-    value += data[8].value / 13 * 706.25;       // % Magic Pen
-    value += data[9].value / 10 * 266.67;       // Ability Haste
-    value += data[10].value / 250 * 350;        // Mana
-    value += data[11].value / 150 * 400;        // Health
-    value += data[12].value / 15 * 300;         // Armor
-    value += data[13].value / 25 * 450;         // Magic Resistance
-    value += data[14].value / 25 * 300;         // Movement Speed
+    value += data[5].value / 5 * 198.33;           // Omnivamp
+    value += data[6].value / 15 * 600;          // Crit Chance
+    value += data[7].value / 20 * 435;          // Ability Power
+    value += data[8].value / 18 * 560;          // Flat Magic Pen
+    value += data[9].value / 13 * 706.25;       // % Magic Pen
+    value += data[10].value / 10 * 266.67;       // Ability Haste
+    value += data[11].value / 250 * 350;        // Mana
+    value += data[12].value / 50 * 250;         // Mana Regen
+    value += data[13].value / 8 * 550;          // Heal & Shield Power
+    value += data[14].value / 150 * 400;        // Health
+    value += data[15].value / 100 * 300;        // Health Regen
+    value += data[16].value / 15 * 300;         // Armor
+    value += data[17].value / 25 * 450;         // Magic Resistance
+    value += data[18].value / 25 * 300;         // Movement Speed
 
 
     const goldValue = (value / cost * 100).toFixed(2);
